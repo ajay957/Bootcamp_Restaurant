e0312493e15475149521c7ba7276b4c231b71776
Ajay957 <ajay74.aa82@gmail.com>
2022-11-22 12:04:01 +0530

    first commit

---
+++ src/Restaurant.java
@@ -0,0 +1,18 @@
+class Items{
+    int itemId;
+    String itemName;
+    int quantity;
+    double price;
+
+    public Items(int itemId,String itemName,int quantity,double price) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.quantity=quantity;
+        this.price=price;
+    }
+
+}
+
+
+public class Restaurant {
+}
